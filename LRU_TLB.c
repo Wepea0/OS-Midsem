cf3b22334040fb5739064175f68490f470ea28d7
Wepea0 <buntugu.adamwaba@ashesi.edu.gh>
2024-02-26 13:41:51 +0000

    Added TLB and LRU algorithm for TLB

--- LRU_TLB.c
+++ LRU_TLB.c
@@ -2,23 +2,162 @@
 #include <string.h>
 #include <stdio.h>
 #include <unistd.h>
+#include "LRU_functions.h"
+#include <stdint.h>
 
-static int TLB[3][3] = {0};
+static int TLB[3][3] = {1};
+static int misses = 0;
+static int hits = 0;
 
-//Returns 1 when the assignment is successful and 0 otherwise
-//Adds a tlb entry to the tlb if there was a miss in accessing this particular PID and page table
-int add_to_tlb(int process_id, int *page_table_ptr){
+static int (*test_ptr)[3][3] = &TLB;
+
+/// @brief Called when there is space in TLB. Adds new entry to tlb and gives it highest priority
+/// @param tlb_index 
+/// @param process_id 
+/// @param page_table_ptr 
+void add_to_tlb_space_exists(int tlb_index, int process_id, int page_table_ptr){
+    //Add new entry
+    printf("Free index - %d\n", tlb_index);
+
+    TLB[tlb_index][0] = process_id;
+    TLB[tlb_index][1] = (intptr_t)page_table_ptr;
+    TLB[tlb_index][2] = sizeof(TLB) - 1; //Give lowest priority
+
+    update_tlb_entry(tlb_index);
+
+}
+
+void add_to_tlb_full(int process_id, int page_table_ptr){
+    int free_index = remove_entry();
+    printf("Free index - %d", free_index);
+
+    TLB[free_index][0] = process_id;
+    TLB[free_index][1] = (intptr_t)page_table_ptr;
+    TLB[free_index][2] = sizeof(TLB) - 1; 
+
+    update_tlb_entry(free_index);
+
+
+}
+
+
+
+int remove_entry(){
+    int lowest_priority = (sizeof(TLB)/sizeof(TLB[0])) - 1;
+    for(int i = 0; i < sizeof(TLB); i++){
+        if(TLB[i][0] == lowest_priority){
+            TLB[i][0] = -1;
+            TLB[i][1] = -1;
+            TLB[i][2] = -1;
+            return i;
+        }
+    }
+    return -1;
+}
+
+int add_to_tlb(int process_id, int page_table_ptr){
+    int free_index = space_exists();
+    if( free_index > -1){
+        add_to_tlb_space_exists(free_index, process_id, page_table_ptr);
+        return 1;
+    }
+    else{
+        add_to_tlb_full(process_id, page_table_ptr);
+        return 1;
+    }
+
     
 }
 
-void main(){
+void update_tlb_entry(int process_index){
+    int current_priority = TLB[process_index][2];
+    if(current_priority == 0){
+        return;
+    }
+    for(int i = 0; i < sizeof(TLB); i++){
+       if( TLB[i][2] < current_priority){
+        TLB[i][2] = TLB[i][2] + 1;
+       }
+    }
+    TLB[process_index][2] = 0;
+    return;
+    //Two situations for an update
+    //TLB entry is already the highest - DONE
+    //TLB entry is not highest
+        //At this point an update to the desired process fra'\
+
+
+}
+
+int entry_exists(int process_id){
+    for(int i = 0; i < sizeof(TLB); i++){
+        if(TLB[i][0] == process_id){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int space_exists(){
+    for(int i = 0; i < sizeof(TLB); i++){
+        if(TLB[i][0] == -1){
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
+// ******************** Utility function *********************
+int ptr_to_integer(int (*page_table_ptr)[3][3]){
+    return (intptr_t)page_table_ptr;
+}
+
+int * integer_to_ptr(long integer){
+    return (int *)integer;
+}
+
+void print_tlb(){
     for(int i = 0; i<3; i++){
         for(int j = 0; j < 3; j++){
-            printf("%d-", TLB[i][j]);
+            printf("%d ~ ", TLB[i][j]);
             
         }
         printf("\n");
     }
+
+}
+
+
+void main(){
+    
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j < 3; j++){
+            TLB[i][j] = i;
+            
+        }
+    }
+
+    printf("\n");
+
+    print_tlb();
+
+    int tlb_index = space_exists();
+    int test_ptr_int = ptr_to_integer(test_ptr);
+    add_to_tlb_full(5, test_ptr_int);
+
+    printf("\n");
+
+    print_tlb();
+
+
+
+   
+
+
+    // printf("%d", space_exists());
+    // printf("\n%d\n", entry_exists(2));
     return;
 
 }
--- LRU_functions.h
+++ LRU_functions.h
@@ -3,9 +3,16 @@
 
 //IMPLEMENTATION NOTES
 //Empty tlb entry will have -1 at each position
+//Highest priority is 0
+//Lowest priority = number of rows in TLB
+//Index 0 = Process ID
+//Index 1 = Page table ptr
+//Index 2 = Priority
 //UPDATE STRATEGY
 //Updates priority of Mmost recently accessed entry to highest (if it is not already)
 //Reduces priority of all other entries (if change is necessary)
+
+int get_entry_from_master_table(int process_id, int pointer_to_master_table);
 
 //Checks whether there is free space in TLB
 //Returns -1 if not and index of free row if it is
@@ -13,26 +20,35 @@ int space_exists();
 
 //Called to check if a particular process entry exists in TLB
 //Returns -1 if it does not and index of row it exists at if it does
-int entry_exists();
+int entry_exists(int process_ID);
 
 //Adds TLB entry if there was a miss in attempting to access it
 //Redirects to one of two other functions depending on whether tlb is full or not
 //Update priorities as necessary
-int add_to_tlb(int process_id, int *page_table_ptr);
+int add_to_tlb(int process_id, int page_table_ptr);
 
 //Called when there is space in TLB
-int add_to_tlb_space_exists(int process_id, int *page_table_ptr);
+void add_to_tlb_space_exists(int tlb_index, int process_id, int page_table_ptr);
 
 //Called when there is no space in TLB
 //Removes least recently used entry and adds new entry
-int add_to_tlb_full(int process_id, int *page_table_ptr);
+void add_to_tlb_full(int process_id, int page_table_ptr);
 
 //Called when updating value for TLB entry that already exists in the table but has just been accessed
 //Updates priority of entry to highest (if it is not already)
 //Reduces priority of all other entries (if change is necessary)
 //Returns 1 if successful, 0 if not
-int update_tlb_entry(int process_id);
+//Uses process_index to access the row of the process_id to update its priority
+void update_tlb_entry(int process_index);
 
+//Remove lowest priority entry from tlb
+int remove_entry();
+
+int ptr_to_integer(int (*page_table_ptr)[3][3]);
+
+int * integer_to_ptr(long integer);
+
+void print_tlb();
 
 
 
