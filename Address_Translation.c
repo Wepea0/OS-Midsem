5f12c340b754c89ad116b5227b19f23c54189dea
Joel Asamoah <joeloseifiifi@gmail.com>
2024-02-27 23:50:57 +0000

    Minor bug fixes

--- Address_Translation.c
+++ Address_Translation.c
@@ -2,32 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_PAGES 16
+#define MAX_PAGES 128
 #define MAX_FRAMES 64
 #define MAX_OFFSET 4
 #define MAX_PROCESS_NUM 8
+#define MAX_PROCESS_PAGES 8
 
 // initializing the memory spaces
-int virtual_memory[MAX_PROCESS_NUM][MAX_PAGES][MAX_OFFSET];
+int virtual_memory[MAX_PAGES][MAX_OFFSET];
 int physical_memory[MAX_FRAMES][MAX_OFFSET];
 
 // initializing page table
 int page_table[MAX_PAGES];
 
 // initializing outer page table to map processes to page tables
-int outer_page_table[MAX_PROCESS_NUM];
-
-// initializing an array of available page tables
-int free_outer_page_tables[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+int *outer_page_table[MAX_PROCESS_NUM];
 
 int first_fit(int a[]);
-void initializeMemory3D(int block, int row, int col, int mem[][row][col]);
 void initializeMemory2D(int row, int col, int mem[][col]);
-int first_fit_page_table (int free_outer_page_table[]);
+void fillVirtualMemory(int start, int num_pages);
 
 int main(void) {
     srand(time(NULL));
-    initializeMemory3D(MAX_PROCESS_NUM, MAX_PAGES, MAX_OFFSET, virtual_memory);
+    initializeMemory2D(MAX_PAGES, MAX_OFFSET, virtual_memory);
     initializeMemory2D(MAX_FRAMES, MAX_OFFSET, physical_memory);
     initializeMemory2D(MAX_PROCESS_NUM, 1, outer_page_table);
 
@@ -37,47 +34,24 @@ int main(void) {
     }
 
     initializeMemory2D(MAX_PAGES, 1, page_table);
+
     int process_number = rand() % MAX_PROCESS_NUM;
-    int num_pages = (rand() % (MAX_PAGES - 1)) + 1; // Generate a random number between 1 and MAX_PAGES
+    int num_pages = (rand() % (MAX_PROCESS_PAGES - 1)) + 1; // Generate a random number between 1 and MAX_PAGES
     int offset_size = rand() % MAX_OFFSET;
 
+    fillVirtualMemory(process_number * MAX_PAGES, num_pages);
     printf("Number of pages: %d\n", num_pages);
-    int page_table_num = first_fit_page_table(free_outer_page_tables);
-    outer_page_table[process_number] = page_table_num;
+    outer_page_table[process_number] = page_table;
 
     for (int i = 0; i < num_pages; i++) {
-        int logical_address[3] = {process_number, page_table_num, i};
+        int logical_address[3] = {process_number, i, offset_size};
         int frame_number = first_fit(logical_address);
-        page_table[i] = frame_number;
+        outer_page_table[process_number][i] = frame_number;
         int physical_address[2] = {frame_number, offset_size};
 
         // printing addresses
-        printf("logical Address: [%d, %d, %d]\tPhysical Address: [%d, %d]\n", process_number, num_pages, offset_size, frame_number, offset_size);
+        printf("logical Address: [%d, %d, %d]\tPhysical Address: [%d, %d]\n", process_number, i, offset_size, frame_number, offset_size);
     }
-
-    /*
-    for (int i = 0; i < 4; i++) {
-        int page_number = rand() % MAX_PAGES;
-        int offset = rand() % (MAX_OFFSET + 1);
-
-        int logical_address[2] = {page_number, offset};
-
-        // Placing the process in the virtual memory
-        for (int i = 0; i < MAX_OFFSET; i++) {
-            virtual_memory[page_number][i] = 1;
-        }
-
-        // assigning frame number to a page number in the page table
-        int frame_number = first_fit(logical_address);
-        page_table[page_number][1] = frame_number;
-
-        // creation of physical address
-        int physical_address[2]  = {frame_number, offset};
-
-        // printing addresses
-        printf("logical Address: [%d, %d]\tPhysical Address: [%d, %d]\n", page_number, offset, frame_number, offset);
-    }*/
-    
 }
 
 int first_fit(int logical_address[]) {
@@ -109,18 +83,6 @@ int first_fit(int logical_address[]) {
     return frame;
 }
 
-int first_fit_page_table (int free_outer_page_table[]) {
-    int ans = 0;
-    for (int i = 0; i < 8; i++) {
-        if (free_outer_page_table[i] == 0) {
-            free_outer_page_table[i] = 1;
-            ans = i;
-            break;
-        }
-    }
-    return ans;
-}
-
 void initializeMemory2D(int row, int col, int mem[][col]) {
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
@@ -129,12 +91,10 @@ void initializeMemory2D(int row, int col, int mem[][col]) {
     }
 }
 
-void initializeMemory3D(int block, int row, int col, int mem[][row][block]) {
-    for (int i = 0; i < block; i++) {
-        for (int j = 0; j < row; j++) {
-            for (int k = 0; k < col; k++) {
-                mem[i][j][k] = 0;
-            }
+void fillVirtualMemory(int start, int num_pages) {
+    for (int i = start; i < start + num_pages; i++) {
+        for (int j = 0; j < MAX_OFFSET; j++) {
+            virtual_memory[i][j] = 1;
         }
     }
 }
